eff4ea3a01ededcd6f1cc8043a1452cbaa27fb04
holybaechu <joonhoo0524@naver.com>
2024-01-27 15:48:50 +0900

    Bump version to 1.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "xyz.holyb"
-version = "1.0.0"
+version = "1.0.2"
 
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 
@@ -17,7 +17,7 @@ labyMod {
         author = "holybaechu"
         description = "Use twitch emotes in Minecraft!"
         minecraftVersion = "1.19.2<*"
-        version = System.getenv().getOrDefault("VERSION", "1.0.0")
+        version = System.getenv().getOrDefault("VERSION", "1.0.2")
     }
 
     minecraft {
